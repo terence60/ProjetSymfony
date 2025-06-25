5b2843c077e91d414cbebdc999ca4131bf4d11a1
Terence Dinguilu <terence.dinguilu@gmail.com>
2025-06-25 17:14:40 +0200

    projet congo

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller; // dossier source
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+// final  = class qui ne veut pas etre heriter
+final class HomeController extends AbstractController // Extends = heriter
+{
+    // dans l'attribut route on y trouve la configuration 
+    // 1er argument (sans terme) : la route (ce qu'on retrouve dans l'url)
+    // serveur + route 
+    // exemple :
+    // en local http://127.0.0.1:8000/home
+    // en ligne www.nomDeDomaine.fr/home
+
+    // 2éme argument 
+
+    // PAS DE DOUBLE QUOTE
+
+   // la méthode render() qui provient de la class AbstractController permet de rendre une vue,
+   // la route retourne une vue c'est à dire un fichier html.twig 
+   // la méthode render() a 2 arguments :
+   // 1e (obligatoire) type : string : le nom du fichier html.twig 
+   // pour info la méthode render() se positionne à la racine du dossier 'templates'
+   // 2éme argument type : array : tableau des données à véhiculer les templates
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+        ]);
+    }
+}
